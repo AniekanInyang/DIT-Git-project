17b28b485c3e2d41767eb0ece9691330949a132d
Mayor <thomasmonte8@gmail.com>
2019-09-24 22:29:23 +0100

    Supervised Machine Learning Project for Monte Thomas

--- Supervised Machine Learning Project for Monte Thomas/knn.py
+++ Supervised Machine Learning Project for Monte Thomas/knn.py
@@ -7,6 +7,9 @@ from sortedcontainers import SortedList
 from util import pre_process_data
 from datetime import datetime
 
+# 3. Code the KNN model class with comments explaining every step 
+
+#Code for the KNN class
 
 class KNN(object):
     def __init__(self, k):
@@ -33,7 +36,7 @@ class KNN(object):
             # print "input:", x
             # print "sl:", sl
 
-            # vote
+            # Empty vote dictionary
             votes = {}
             for _, v in sl:
                 # print "v:", v
@@ -41,13 +44,14 @@ class KNN(object):
             # print "votes:", votes, "true:", Ytest[i]
             max_votes = 0
             max_votes_class = -1
-            for v,count in iteritems(votes):
+            for v,count in iteritems(votes): #counting votes
                 if count > max_votes:
                     max_votes = count
                     max_votes_class = v
             y[i] = max_votes_class
+        #predictions    
         return y
-
+    #scoring the predicted values 
     def score(self, X, Y):
         P = self.predict(X)
         return np.mean(P == Y)
@@ -60,6 +64,8 @@ if __name__ == '__main__':
     Xtest, Ytest = X[Ntrain:], Y[Ntrain:]
     train_scores = []
     test_scores = []
+
+# 4. For K = 1 to 8, give a list of train accuracy scores and test accuracy stores     
     ks = (1,2,3,4,5,6,7,8)
     for k in ks:
         print("\nk =", k)
@@ -80,8 +86,95 @@ if __name__ == '__main__':
         test_scores.append(test_score)
         print("Time to compute test accuracy:", (datetime.now() - t0), "Test size:", len(Ytest))
 
+# 5. Plot a graph of the test scores against k and train scores against k correctly labeled 
+
     plt.plot(ks, train_scores, label='train scores')
     plt.plot(ks, test_scores, label='test scores')
     plt.legend()
     plt.show()
+
+
+
+# 6. What do you notice about k from the graph? Which do you think is the suitable k? and why? 
+    #The initial values of k= 1,2 gave train accuracy of 100% which does not generalize very well.
+    #Highervalues of k gave an averegely increasing train & test accuracy. The best combination was produced by k value of 8
+
+
+
+
+# 7. Code the Perceptron model with comments explaining every step (Recall you have to add a condition in your preprocessing function that changed the labels (N= -1, Y = 1) for the Perceptron model 
+
+class Perceptron:
+    def fit(self, X, Y, learning_rate=1.0, epochs=1000):
+        # randomly initializing the weights and setting b to zero
+        D = X.shape[1]
+        self.w = np.random.randn(D)
+        self.b = 0
+        print("first w: ", self.w)
+
+        N = len(Y)
+        costs = []
+        for epoch in range(epochs):
+            # making predictions
+            Yhat = self.predict(X)
+            # sieving out misclassified samples
+            incorrect = np.nonzero(Y != Yhat)[0]
+            if len(incorrect) == 0:
+                # breaking out if all classified correctly
+                break
+
+            i = np.random.choice(incorrect)
+            # updating values of w and b with randomly selected Y and/or X with learning rate
+            self.w =self.w + learning_rate * Y[i] * X[i]
+            self.b =self.b + learning_rate * Y[i]
+
+            # cost is incorrect rate
+            c = len(incorrect) / float(N)
+            costs.append(c)
+        print("final w:", self.w, "final b:", self.b, "epochs:", (epoch + 1), "/", epochs)
+        plt.plot(costs)
+        plt.title("Graph of Costs for Perceptron model")
+        plt.show()
+
+    # Predicting Y labels
+    def predict(self, X):
+        return np.sign(X.dot(self.w) + self.b)
+
+    # Scoring the prediction
+    def score(self, X, Y):
+        P = self.predict(X)
+        return np.mean(P == Y)
+        
+
+# 8. Plot a graph of the costs 
+
+if __name__ == '__main__':
+    X, Y = pre_process_data(model='perceptron')
+
+    Ntrain = len(Y) // 2
+    Xtrain, Ytrain = X[:Ntrain], Y[:Ntrain]
+    Xtest, Ytest = X[Ntrain:], Y[Ntrain:]
+
+    model = Perceptron()
+learning_rate = [0.001,0.01,0.1,1]
+for rates in learning_rate:
+    model.fit(Xtrain, Ytrain, learning_rate= rates, epochs=30000)
+    print("Train accuracy:", model.score(Xtrain, Ytrain))
+    print("Train size:", len(Ytrain))
+    print("Test accuracy:", model.score(Xtest, Ytest))
+    print("Test size:", len(Ytest))
+
+
+# 9. Why did you choose your learning rate and epochs?
+
+    # I created a list of possible learning rates between the usual lowest and highest learning rates of 0.001 and 1
+    # I went further to iterate throught list and printing the train and test accuracy in each scenerio with a gradual increase in the number of epochs
+    # I observed that I got the best Train & Test accuracy in each successive iteration with the when the learning rate is 0.01
+    # This made me decided to work with the learning rate of 0.01 and the epoch value of 30000
+
+
+# 10. What can you do to increase train and test accuracy? 
+    # Tweaking the size of the train-test split can possibly help increase accuracy.
+    # Adjusting the learning rates/epochs can affect the accuracy on the data output
+    # Randomly assign your values in the model to avoid batch selection that can derail results gotten
 
--- Supervised Machine Learning Project for Monte Thomas/util.py
+++ Supervised Machine Learning Project for Monte Thomas/util.py
@@ -6,56 +6,80 @@ from sklearn import preprocessing
 
 # 1. What is the difference in the implementation of the Bayes and Naïve Bayes model
 
-#The Naive Bayes assumes all input features are independent/not correlated while Bayes classifier works with the fact that there's a correlation
-#between all input features.
+    #The Naive Bayes assumes all input features are independent/not correlated while Bayes classifier works with the fact that there's a correlation
+    #between all input features.
 
 
 # 2. Create a python file called util.py which hosts a pre_process_data() function that takes in the data and preprocesses it
 
 
-#a. Read in the data 
+# The block of code below represents the answer to the following questions:
+# a. Read in the data 
+# b. Change the labels (for knn use ; N = 0, Y = 1) and (for perceptron use N = -1, Y = 1)
+# c. Drop Dependents, LoanID and LoanAmountTerm columns 
+# e. Change Property column (Rural = 0, Semiurban = 0.5, Urban = 1) 
+# f. Drop all rows with missing data 
+ 
 def pre_process_data(limit=None, model='knn'):
     print("Reading Data from source and pre-processing it ...")
     df1 = pd.read_csv('C:/Users/USER/Downloads/loan_data.csv', header='infer').dropna()
-    df = df1.drop(["Loan_ID","Loan_Amount_Term","Dependents"],inplace = True, axis = 1)
-    data = df.values
-
+    df = df1.drop(["Loan_ID","Loan_Amount_Term","Dependents"], axis = 1)
     
     
-#b. Change the labels (N = 0, Y = 1) if it's knn and (N = -1, Y = 1) if it's perceptron
+    
     
     if model == 'knn':
         df.Loan_Status[df.Loan_Status == 'N'] = 0
         df.Loan_Status[df.Loan_Status == 'Y'] = 1
 
+        df.Married[df.Married == 'No'] = 0
+        df.Married[df.Married == 'Yes'] = 1
+        
+        df.Self_Employed[df.Self_Employed == 'No'] = 0
+        df.Self_Employed[df.Self_Employed == 'Yes'] = 1
+        
+        
+        df.Property_Area[df.Property_Area == 'Rural'] = 0
+        df.Property_Area[df.Property_Area == 'Semiurban'] = 0.5
+        df.Property_Area[df.Property_Area == 'Urban'] = 1
+        
+        df.Gender[df.Gender == 'Male'] = 0
+        df.Gender[df.Gender == 'Female'] = 1
+        
+        df.Education[df.Education == 'Graduate'] = 1
+        df.Education[df.Education == 'Not Graduate'] = 0
+
     elif model == 'perceptron':
         df.Loan_Status[df.Loan_Status == 'N'] = -1
         df.Loan_Status[df.Loan_Status == 'Y'] =  1
         
-    df.Married[df.Married == 'No'] = 0
-    df.Married[df.Married == 'Yes'] = 1
+        df.Married[df.Married == 'No'] = 0
+        df.Married[df.Married == 'Yes'] = 1
+        
+        df.Self_Employed[df.Self_Employed == 'No'] = 0
+        df.Self_Employed[df.Self_Employed == 'Yes'] = 1
+        
+        
+        df.Property_Area[df.Property_Area == 'Rural'] = 0
+        df.Property_Area[df.Property_Area == 'Semiurban'] = 0.5
+        df.Property_Area[df.Property_Area == 'Urban'] = 1
+        
+        df.Gender[df.Gender == 'Male'] = 0
+        df.Gender[df.Gender == 'Female'] = 1
+        
+        df.Education[df.Education == 'Graduate'] = 1
+        df.Education[df.Education == 'Not Graduate'] = 0
     
-    df.Self_Employed[df.Self_Employed == 'No'] = 0
-    df.Self_Employed[df.Self_Employed == 'Yes'] = 1
     
     
-    df.Property_Area[df.Property_Area == 'Rural'] = 0
-    df.Property_Area[df.Property_Area == 'Semiurban'] = 0.5
-    df.Property_Area[df.Property_Area == 'Urban'] = 1
-    
-    df.Gender[df.Gender == 'Male'] = 0
-    df.Gender[df.Gender == 'Female'] = 1
-    
-    df.Education[df.Education == 'Graduate'] = 1
-    df.Education[df.Education == 'Not Graduate'] = 0
-    
-    
-    
+    data = df.values
+
+    np.random.shuffle(data)
     shuffle(data)
-    X = data[:, :-1].astype(np.float)
-    Y = data[:, -1].astype(np.int)
-       
-    
+    X = data[:, :-1]
+    Y = data[:, -1]
+
+# d. Normalize the LoanAmount, ApplicantIncome and CoapplicantIncome columns using the MinMax Scaler so that it’s between 0 and 1 
     
     min_max_scaler = preprocessing.MinMaxScaler()
     data[:, [4, 5, 6]] = min_max_scaler.fit_transform(data[:, [4, 5, 6]])
